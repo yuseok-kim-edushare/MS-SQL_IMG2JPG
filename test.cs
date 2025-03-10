588992a636813acf75d0022163bd1fd60e963996
김유석(Yu Seok Kim) <yuseok.kim@vilac.co.kr>
2025-03-10 18:39:07 +0900

    🚧 create test.cs
    fill real test code into this file and add conditional compile option added like https://github.com/yuseok-kim-edushare/simple-.net-Crypting-For-PowerBuilder/blob/main/NET8/test.cs

---
+++ test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Image2Jpg;
+
+namespace Image2Jpg.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
